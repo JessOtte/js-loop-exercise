5d415e0007d0e9e9ff14699003809e51e3042782
semiottematic <40188802+semiottematic@users.noreply.github.com>
2019-02-10 14:40:08 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,90 @@
+// FOR LOOP
+// FOR LOOP
+// FOR LOOP
+// FOR LOOP
+// FOR LOOP
+// FOR LOOP
+// FOR LOOP
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//   }
+
+//   for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+//   }
+
+
+
+
+// WHILE LOOP
+// WHILE LOOP
+// WHILE LOOP
+// WHILE LOOP
+// WHILE LOOP
+// WHILE LOOP
+// WHILE LOOP
+
+
+
+// let i = 1;
+// while (i < 11) {
+// console.log(i);
+// i++;
+// }
+
+
+// let x = 10;
+// while (x >= 1) {
+//     console.log(x);
+//     x--;
+// }
+
+
+
+
+// DO WHILE LOOP
+// DO WHILE LOOP
+// DO WHILE LOOP
+// DO WHILE LOOP
+// DO WHILE LOOP
+// DO WHILE LOOP
+// DO WHILE LOOP
+// DO WHILE LOOP
+
+
+// let z = 0;
+// do {
+//   z += 1;
+//   console.log(z);
+// } while (z < 10);
+
+
+// let y = 11;
+// do {
+//   y--;
+//   console.log(y);
+// } while (y > 1);
+
+
+
+
+
+// COUNT USING AN ARRAY
+// COUNT USING AN ARRAY
+// COUNT USING AN ARRAY
+// COUNT USING AN ARRAY
+// COUNT USING AN ARRAY
+// COUNT USING AN ARRAY
+// COUNT USING AN ARRAY
+// COUNT USING AN ARRAY
+
+
+
+  let navItems =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  
+  for (let i = 0; i < navItems.length; i++) {
+    console.log(navItems[i]);
+  }
+  
